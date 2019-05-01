847e6cb7409c18793b26ee0e02b09757d24f2d10
TheRigidNinja <test1@Brians-MBP.gateway>
2019-05-01 11:20:03 +1000

    Updates.2 fixed key problem

--- Firebase_DB/Authentifacation.js
+++ Firebase_DB/Authentifacation.js
@@ -1,12 +1,5 @@
 const firebase = require("firebase");
-const config = {
-    apiKey: "AIzaSyBa292saQfeQuy-uSq0kur6Zu2UeUdmfuQ",
-    authDomain: "chatapp-66db3.firebaseapp.com",
-    databaseURL: "https://chatapp-66db3.firebaseio.com",
-    projectId: "chatapp-66db3",
-    storageBucket: "chatapp-66db3.appspot.com",
-    messagingSenderId: "545363742777"
-  };
+const config = require("configKey").configKey;
 firebase.initializeApp(config);
 
 async function AuthtoFirebase(LoginCredential){
---
+++ Firebase_DB/configKey.js
@@ -0,0 +1,18 @@
+
+const config  = ()=>{
+    const config = {
+        apiKey: "AIzaSyBa292saQfeQuy-uSq0kur6Zu2UeUdmfuQ",
+        authDomain: "chatapp-66db3.firebaseapp.com",
+        databaseURL: "https://chatapp-66db3.firebaseio.com",
+        projectId: "chatapp-66db3",
+        storageBucket: "chatapp-66db3.appspot.com",
+        messagingSenderId: "545363742777"
+    };
+
+    return config
+}
+
+
+module.exports={
+    configKey: config
+}
