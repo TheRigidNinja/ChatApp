d34d9a890ca7b39a57b8fbd39f80019d8921a0fa
Brian Shisanya <brian.shisanya2000@gmail.com>
2019-05-05 00:04:13 +1000

    Update Authentifacation.js

--- Firebase_DB/Authentifacation.js
+++ Firebase_DB/Authentifacation.js
@@ -1,12 +1,12 @@
 const firebase = require("../node_modules/firebase");
 
 const config = {
-    apiKey: "AIzaSyBa292saQfeQuy-uSq0kur6Zu2UeUdmfuQ",
-    authDomain: "chatapp-66db3.firebaseapp.com",
-    databaseURL: "https://chatapp-66db3.firebaseio.com",
-    projectId: "chatapp-66db3",
-    storageBucket: "chatapp-66db3.appspot.com",
-    messagingSenderId: "545363742777"
+    apiKey: "----------",
+    authDomain: "----------",
+    databaseURL: "----------",
+    projectId: "----------",
+    storageBucket: "----------",
+    messagingSenderId: "----------"
 };
 
 firebase.initializeApp(config);
